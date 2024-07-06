3d78f332db5944d560ea41f2ea2674d57a0e8034
ErickVera8 <etrickvera@gmail.com>
2024-07-06 17:05:51 -0500

    Proyecto con las clases Usuario y Articulo

---
+++ src/Articulo.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Erick
+ */
+public class Articulo {
+    private String titulo;
+    private String resumen;
+    private String contenido;
+    private String palabrasClave;
+    public Articulo(String titulo,String resumen,String contenido,String palabraClave ){
+        
+        this.resumen=resumen;
+        this.contenido=contenido;
+        this.palabrasClave=palabraClave;
+    }
+    public String getTitulo(){
+        return titulo;
+    }
+    public void setTitulo(String titulo){
+        this.titulo=titulo;
+    }
+    public String getResumen(){
+        return resumen;
+    }
+    public void setResumen(String resumen){
+        this.resumen=resumen;
+    }
+    public String getContenido(){
+        return contenido;
+    }
+    public void setContenido(String contenido){
+        this.contenido=contenido;
+    }
+    public String getPalabrasClave(){
+        return palabrasClave;
+    }
+    public void setPalabrasClave(String palabrasClave){
+        this.palabrasClave=palabrasClave;
+    }
+    
+}
